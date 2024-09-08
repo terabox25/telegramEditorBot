429d5c5f7ab4436f6db49b692cae1c62166e26f5
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-09-08 14:59:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Telegram Bot Token
+TOKEN = '5645711998:AAE8oAHzKi07iqcydKPnuFjzknlVa2MxxUQ'
+
+# Additional configurations if needed in the future
+# API_ID = 'Your API ID'
+# API_HASH = 'Your API Hash'
